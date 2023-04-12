5fe659e74d7381d4a2791c40f746224684b63663
infcode <infcode@163.com>
2023-04-12 00:41:32 -0400

    make human_render test terminate quickly

--- tests/human_render.py
+++ tests/human_render.py
@@ -2,15 +2,16 @@ import gym
 import porlygon.env
 
 if __name__ == "__main__":
-    env = gym.make("DrawPolygon-v0", render_mode = "human")
-    env.reset(seed = 42)
-    
+    env = gym.make("DrawPolygon-v0", render_mode="human")
+    env.reset(seed=42)
+
     action_space = env.action_space
-    for i in range(100):
+    for i in range(2):
         term = False
         trunc = False
         while not term and not trunc:
             env.render()
-            obs, reward, term, trunc, info =  env.step(action_space.sample())
+            obs, reward, term, trunc, info = env.step(action_space.sample())
         env.render()
         env.reset()
+    env.close()
