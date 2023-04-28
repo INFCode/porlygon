9f98631b8a83af4184d058ede936a98a1a655cfa
infcode <infcode@163.com>
2023-04-28 04:09:35 -0400

    remove human render mode from speed_test

--- tests/speed_test.py
+++ tests/speed_test.py
@@ -1,18 +1,20 @@
-from timeit import timeit 
+from timeit import timeit
 import gym
 import porlygon.env
 
-env = gym.make("DrawPolygon-v0", render_mode = "human")
-env.reset(seed = 42)
+env = gym.make("DrawPolygon-v0")
+env.reset(seed=42)
 
 action_space = env.action_space
+
 
 def env_full_loop():
     term = False
     trunc = False
     while not term and not trunc:
-        _, _, term, trunc, _ =  env.step(action_space.sample())
+        _, _, term, trunc, _ = env.step(action_space.sample())
     env.reset()
 
+
 if __name__ == "__main__":
-    print(timeit("env_full_loop()", number = 1000, globals=globals())) # gives 223.23
+    print(timeit("env_full_loop()", number=1000, globals=globals()))  # gives 223.23
